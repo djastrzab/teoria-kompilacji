650ffa65b1816954b747d1856baa01dd16a742e2
Observer46 <microplayerpl@gmail.com>
2020-10-14 16:42:05 +0200

    Added most of basic stuff

--- scanner.py
+++ scanner.py
@@ -1,32 +1,53 @@
 import ply.lex as lex
 
-reserved = {'zeros' : 'ZEROS'}
+reserved = {'zeros' : 'ZEROS',
+            'eye' : 'EYE',
+            'ones' : 'ONES',
+            'break' : 'BREAK',
+            'continue' : 'CONTINUE',
+            'return' : 'RETURN',
+            'print' : 'PRINT',
+            'if' : 'IF',
+            'else' : 'ELSE',
+            'for' : 'FOR',
+            'while' : 'WHILE'}
 
-tokens = ['COMMENT', 'MPLUS', 'MMINUS', 'MTIMES', 'MDIVIDE', 'NUMBER',
-          'ASSIGNPLUS', 'ASSIGNMINUS', 'ASSIGNTIMES', 'ASSIGNDIVIDE', 'ID'] + list(reserved.values())
+tokens = ['COMMENT', 'MPLUS', 'MMINUS', 'MTIMES', 'MDIVIDE', 'INTNUMBER',
+          'ASSIGNPLUS', 'ASSIGNMINUS', 'ASSIGNTIMES', 'ASSIGNDIVIDE',
+          'LEQ', 'EQ', 'GEQ', 'NEQ', 'FLOATNUMBER', 'STRING', 'ID'] + list(reserved.values())
 
-t_COMMENT = r'\#.*'
-t_MPLUS = r'.\+'
-t_MMINUS = r'.-'
-t_MTIMES = r'.\*'
-t_MDIVIDE = r'./'
+t_COMMENT = r'\#.*'         # Jeszcze pomijamy komentarzy
+t_MPLUS = r'\.\+'
+t_MMINUS = r'\.-'
+t_MTIMES = r'\.\*'
+t_MDIVIDE = r'\./'
 t_ASSIGNPLUS = r'\+='
 t_ASSIGNMINUS = r'-='
 t_ASSIGNTIMES = r'\*='
 t_ASSIGNDIVIDE = r'/='
+t_LEQ = r'<='
+t_EQ = r'=='
+t_GEQ = r'>='
+t_NEQ = r'!='
 
 
-
-literals = "+-*/()[]{}="
-
+literals = "+-*/()[]{}=<>,;:'"
 
 
-
-def t_NUMBER(t):
+def t_INTNUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
+def t_FLOATNUMBER(t):       # Jeszcze nie wiem co zrobic z 60.52E2 (16 linijka)
+    r'\.\d+|\d+\.\d*'
+    t.value = float(t.value)
+    return t
+
+def t_STRING(t):
+    r'".*"'
+    t.value = str(t.value)
+    return t
 
 def t_ID(t):
     r'[a-zA-Z_]\w*'
