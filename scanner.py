af02e84e48aac5faa796f0e6a05fe1818c83d8b7
DHawk98 <44686772+DHawk98@users.noreply.github.com>
2020-10-16 21:01:46 +0200

    poprawione wykrywanie
    zwraca dobrą ilość znaków, ale nie zwraca ostatniej linii w której wystąpił błąd

--- scanner.py
+++ scanner.py
@@ -71,21 +71,24 @@ def t_newline(t):
 
 tmp_line = 0
 tmp_error_counter = 0
+tmp_str_unknown_ch = ""
 
 
 def t_error(t):
-    global tmp_line, tmp_error_counter;
-    if t.lexer.lineno == tmp_line:
-        t.lexer.skip(1)
-        tmp_error_counter += 1
-    else:
-        if (tmp_error_counter > 1):
-            print( str(tmp_error_counter) + " Illegal characters at line '%s'" % t.lexer.lineno)
-        else:
-            print("Illegal character at line '%s'" % t.lexer.lineno)
+    global tmp_line, tmp_error_counter, tmp_str_unknown_ch
+    if tmp_line == 0:
         tmp_line = t.lexer.lineno
-        tmp_error_counter = 0
-
+        tmp_error_counter = 1
+    else:
+        if t.lexer.lineno > tmp_line:
+            if tmp_error_counter > 1:
+                print(str(tmp_error_counter) + " Illegal characters at line '%s'" % tmp_line)
+            else:
+                print("Illegal character at line '%s'" % tmp_line)
+            tmp_line = t.lexer.lineno
+            tmp_error_counter = 1
+        else:
+            tmp_error_counter += 1
     t.lexer.skip(1)
 
 
