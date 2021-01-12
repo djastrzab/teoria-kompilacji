a76b5fa9578ffe422c185d7d51692353faa8256d
DHawk98 <44686772+DHawk98@users.noreply.github.com>
2021-01-12 09:35:20 +0100

    fix

--- lab5/main.py
+++ lab5/main.py
@@ -1,25 +1,31 @@
 import sys
 import ply.yacc as yacc
 import Mparser
-import scanner
 from TreePrinter import TreePrinter
 from TypeChecker import TypeChecker
+import Interpreter
 
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "opers.txt"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
         sys.exit(0)
 
+    Mparser = Mparser()
     parser = yacc.yacc(module=Mparser)
     text = file.read()
-    ast = parser.parse(text, lexer=scanner.lexer)
-    # if not Mparser.incorrect_input:
-    #     ast.printTree()
+
+    ast = parser.parse(text, lexer=Mparser.scanner)
 
     # Below code shows how to use visitor
     typeChecker = TypeChecker()
     typeChecker.visit(ast)  # or alternatively ast.accept(typeChecker)
+
+    ast.accept(Interpreter())
+    # in future
+    # ast.accept(OptimizationPass1())
+    # ast.accept(OptimizationPass2())
+    # ast.accept(CodeGenerator())
